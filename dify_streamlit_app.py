3883133d991aee96043cc37d4b96140317234911
kishimon02 <kisy0222@outlook.jp>
2025-01-05 11:39:27 +0900

    Update dify_streamlit_app.py

--- dify_streamlit_app.py
+++ dify_streamlit_app.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 dify_api_key = st.secrets["DIFY_API_KEY"]   # Streamlitのsecrets機能を使って、Dify APIのAPIキーを安全に取得
-url = 'https://api.dify.ai/v1'    # Dify APIのエンドポイント（APIの機能が利用できるURL）
+url = 'https://api.dify.ai/v1/chat-messages'    # Dify APIのエンドポイント（APIの機能が利用できるURL）
 
 st.title('Kishimon AIのお悩み相談室')   # タイトルを表示
 
